1adf64e42b4845d1a78f6b2124e4c6bf9480fab6
heeseungp <heeseung5@gmail.com>
2017-06-22 17:24:01 -0400

    Working on the follow up of sum lists

--- linkedlist/LinkedList.h
+++ linkedlist/LinkedList.h
@@ -38,7 +38,8 @@ class LinkedList {
 		// Partition: Write code to partition a linked list around a value x, such that all nodes less than x come before all nodes greater or equal to x.
 		void partition(int value);
 
-		
+
+
 
 	private:
 		Node* head;
--- linkedlist/main.cpp
+++ linkedlist/main.cpp
@@ -4,15 +4,54 @@
 using std::cout;
 using std::endl;
 
+// Sum Lists: Write a function that adds the two numbers and returns the sum as a linked list	
+LinkedList iter_sumlists(LinkedList* first, LinkedList* second) {
+	//iterative version
+	LinkedList resultList;
+	Node* first_temp = first->get_head_node();
+	Node* second_temp = second->get_head_node();
+	int result = 0;
+	int carry = 0;
+	while(first_temp != NULL && second_temp != NULL) {
+		result = carry;
+		if(first_temp != NULL) {
+			result += first_temp->get_data();
+			first_temp = first_temp->nextNode();			
+		} 
+		if(second_temp != NULL) {
+			result += second_temp->get_data();
+			second_temp = second_temp->nextNode();
+		}
+		carry = result / 10;
+		result = result % 10;
+		resultList.addtoTail(result);
+		result = 0;
+	}
+	return resultList;
+}
+// Sum Lists Follow Up
+LinkedList sumlists(LinkedList* first, LinkedList* second) {
+	
+}
+
 int main() {
 
-	LinkedList hi;
-	hi.addtoTail(1);
-	hi.addtoTail(2);
-	hi.print();
-	hi.partition(5);
-	hi.print();
+	LinkedList first;
+	LinkedList second;
+
+	first.addtoTail(7);	
+	first.addtoTail(1);
+	first.addtoTail(6);
+	second.addtoTail(5);	
+	second.addtoTail(9);
+	second.addtoTail(2);
+
+	first.print();
+	second.print();
+
+
+	sumlists(&first, &second).print();
 
 
 	return 0;
-}
+} 
