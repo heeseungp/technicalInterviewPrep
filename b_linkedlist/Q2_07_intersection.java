1357c95b802abc03c81d6a8c732e08d3efe3a345
heeseungp <heeseung5@gmail.com>
2018-01-17 18:10:56 -0500

    Complete interesection

--- b_linkedlist/Q2_07_intersection.java
+++ b_linkedlist/Q2_07_intersection.java
@@ -13,3 +13,109 @@
 			If found, return the intersecting node
 
 */
+
+package b_linkedlist;
+import java.util.*;
+
+public class Q2_07_intersection {
+
+	public static ListNode intersection(ListNode first, ListNode second) {
+
+		if(tail(first) != tail(second)) {
+			return null;
+		}
+
+		int firstSize = sizeofList(first);
+		int secondSize = sizeofList(second);
+
+		if(firstSize == 0 || secondSize == 0) {
+			return null;
+		}
+
+		int steps = firstSize-secondSize;
+		if(steps > 0) {
+			first = moveForward(first, steps);
+		} else if(steps < 0){
+			second = moveForward(second,steps);
+		}
+
+		// same length
+		while(first != second) {
+			first = first.next;
+			second = second.next;
+		}
+
+		return first;
+	}
+
+	public static ListNode tail(ListNode head) {
+		while(head != null && head.next != null) {
+			head = head.next;
+		}
+
+		return head;
+	}
+
+	public static ListNode moveForward(ListNode head, int count) {
+		while(head != null && count > 0) {
+			head = head.next;
+			count--;
+		}
+		return head;
+	}
+
+	public static int sizeofList(ListNode head) {
+		int count = 0;
+		while(head != null) {
+			count++;
+			head = head.next;
+		}
+		return count;
+	}
+
+
+	public static void printListNode(ListNode head) {
+		System.out.println("");
+		while(head != null) {
+			System.out.print(head.data);
+			if(head.next != null) {
+				System.out.print(" -> ");
+			}
+			head = head.next;
+		}
+		System.out.println("");
+		System.out.println("");
+	}
+
+
+	public static void main(String[] args) {
+		ListNode a = new ListNode(1);
+		ListNode b = new ListNode(2);
+		ListNode c = new ListNode(3);
+		ListNode d = new ListNode(1);
+		ListNode e = new ListNode(5);
+		
+		ListNode f = new ListNode(6);
+		ListNode g = new ListNode(1);
+		ListNode h = new ListNode(2);
+		ListNode i = new ListNode(3);
+		ListNode j = new ListNode(1);
+		ListNode k = new ListNode(5);
+
+		a.next = b;
+		b.next = c;
+		c.next = d;
+		d.next = e;
+
+		f.next = g;
+		g.next = h;
+		h.next = d;
+
+		printListNode(a);
+		printListNode(f);
+
+		System.out.println(intersection(a, f));
+
+		System.out.println("The answer (intersection node): " + d);
+	}
+}
